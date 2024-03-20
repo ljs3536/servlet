88041fcdcf569b553763d1158f124b0dd8c365f5
ljs3536 <wlstjr320@naver.com>
2024-03-21 00:19:36 +0900

    Hello 서블릿 2

--- src/main/java/hello/servlet/basic/HelloServlet.java
+++ src/main/java/hello/servlet/basic/HelloServlet.java
@@ -13,6 +13,11 @@ public class HelloServlet extends HttpServlet {
 
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println("HelloServlet.service");;
+        System.out.println("HelloServlet.service");
+        System.out.println("req = " + req);
+        System.out.println("resp = " + resp);
+
+        String username = req.getParameter("username");
+        System.out.println("username = " + username);
     }
 }
